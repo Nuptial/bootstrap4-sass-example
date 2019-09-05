6519aa6f64e582690a8e082787462b58c2919536
Nuptial <alican.camlibel91@gmail.com>
2019-09-05 23:00:18 +0300

    second

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 $('body').on('click','.mobile-fixed-button',function(){
     $('.left-side-menu').addClass('active');
     $('.body-overlay').addClass('active');
+    $(this).addClass('active');
 });
 
-$('body').on('click','.body-overlay',function(){
+$('body').on('click','.body-overlay, .mobile-fixed-button.active',function(){
     $('.body-overlay').removeClass('active');
     $('.left-side-menu').removeClass('active');
+    $('.mobile-fixed-button').removeClass('active');
 });
